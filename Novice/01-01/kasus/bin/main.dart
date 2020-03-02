a5afd8b5a82d7b31465e1eb99b5e897a9aabcf0a
nununganggriani <61130727+nununganggriani@users.noreply.github.com>
2020-03-02 20:49:25 +0700

    Update main.dart

--- Novice/01-01/kasus/bin/main.dart
+++ Novice/01-01/kasus/bin/main.dart
@@ -1,5 +1,65 @@
-import 'package:kasus/kasus.dart' as kasus;
+//import 'package:kasus/kasus.dart' as kasus;
 
-void main(List<String> arguments) {
-  print('Hello World: ${kasus.calculate()}!');
+//void main(List<String> arguments) {
+  //print('Hello World: ${kasus.calculate()}!');
+//}
+
+void main() {
+  //HELLO
+  //print('Hello, World!');
+//}
+
+//VARIABLES
+var name = 'Nunungggg';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+print(name);
 }
+
+//*CONTROL FLOW
+  //var year = 2020;
+ // var flybyObjects=[];
+//if (year >= 2001) {
+  //print('21st century');
+//} else if (year >= 1901) {
+ // print('20th century');
+//}
+
+//for (var object in flybyObjects) {
+ // print(object);
+//}
+
+//for (int month = 1; month <= 12; month++) {
+  //print(month);
+//}
+
+//while (year < 2016) {
+  //year += 1;
+//}
+//}
+
+//FUNCTIONS
+  //var result = fibonacci(20); 
+  //print(result);
+//}
+//int fibonacci(int n) {
+  //if (n == 0 || n == 1) return n;
+  //return fibonacci(n - 1) + fibonacci(n - 2);
+//}
+
+//COMMENTS
+// This is a normal, one-line comment.
+
+/// This is a documentation comment, used to document libraries,
+/// classes, and their members. Tools like IDEs and dartdoc treat
+/// doc comments specially.
+
+/* Comments like these are also supported. */
+//}
+
+//IMPORTS
