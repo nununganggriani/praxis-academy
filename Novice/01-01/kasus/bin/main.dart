03472765deae21ed51ab139b777c96889740140f
nununganggriani <61130727+nununganggriani@users.noreply.github.com>
2020-03-03 20:42:28 +0700

    Update main.dart

--- Novice/01-01/kasus/bin/main.dart
+++ Novice/01-01/kasus/bin/main.dart
@@ -1,49 +1,45 @@
-import '../lib/kasus.dart' as kasus;
+import '../lib/Kasus.dart' as Kasus;
 
 void main(List<String> arguments) {
-  print('Hello World: ${kasus.calculate()}!');
+  print('Hello world: ${Kasus.calculate()}!');
+
+  var name = 'Voyager I';
+    var year = 1977;
+    var antennaDiameter = 3.7;
+    var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+    var image = {
+      'tags': ['saturn'],
+      'url': '//path/to/saturn.jpg'
+};
+    print(image);
+    print(name);
+
+            if (year >= 2001) {
+          print('21st century');
+        } else if (year >= 1901) {
+          print('20th century');
+        }
+
+        for (var object in flybyObjects) {
+          print(object);
+        }
+
+        for (int month = 1; month <= 12; month++) {
+          print(month);
+        }
+
+        while (year < 2016) {
+          year += 1;
+        }
+
+        int fibonacci(int n) {
+        if (n == 0 || n == 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+       var result = fibonacci(20);
+       print(result);
+
+       flybyObjects.where((name) => name.contains('turn')).forEach(print);
+
 }
-
-//CONTROL
-//main(){
-  //var year = 2020;
-  //var flybyObjects=[];
-//if (year >= 2001) {
-  //print('21st century');
-//} else if (year >= 1901) {
-  //print('20th century');
-//}
-
-//for (var object in flybyObjects) {
-  //print(object);
-//}
-
-//for (int month = 1; month <= 12; month++) {
-  //print(month);
-//}
-
-//while (year < 2016) {
-  //year += 1;
-//}
-//}
-
- //FUNCTIONS
- //var result = fibonacci(20); 
-  //print(result);
-//}
-//int fibonacci(int n) {
-  //if (n == 0 || n == 1) return n;
-  //return fibonacci(n - 1) + fibonacci(n - 2);
-//}
-
-//VARRIABLES
-//var name = 'Voyager I';
-//var year = 1977;
-//var antennaDiameter = 3.7;
-//var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-//var image = {
-  //'tags': ['saturn'],
-  //'url': '//path/to/saturn.jpg'
-//};
-//print(name);
-//}
