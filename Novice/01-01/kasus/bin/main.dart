395a69d970d38cf1c5ec3725aa03a9acf8e92e83
nununganggriani <61130727+nununganggriani@users.noreply.github.com>
2020-03-03 19:40:28 +0700

    Update main.dart

--- Novice/01-01/kasus/bin/main.dart
+++ Novice/01-01/kasus/bin/main.dart
@@ -3,3 +3,47 @@ import '../lib/kasus.dart' as kasus;
 void main(List<String> arguments) {
   print('Hello World: ${kasus.calculate()}!');
 }
+
+//CONTROL
+//main(){
+  //var year = 2020;
+  //var flybyObjects=[];
+//if (year >= 2001) {
+  //print('21st century');
+//} else if (year >= 1901) {
+  //print('20th century');
+//}
+
+//for (var object in flybyObjects) {
+  //print(object);
+//}
+
+//for (int month = 1; month <= 12; month++) {
+  //print(month);
+//}
+
+//while (year < 2016) {
+  //year += 1;
+//}
+//}
+
+ //FUNCTIONS
+ //var result = fibonacci(20); 
+  //print(result);
+//}
+//int fibonacci(int n) {
+  //if (n == 0 || n == 1) return n;
+  //return fibonacci(n - 1) + fibonacci(n - 2);
+//}
+
+//VARRIABLES
+//var name = 'Voyager I';
+//var year = 1977;
+//var antennaDiameter = 3.7;
+//var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+//var image = {
+  //'tags': ['saturn'],
+  //'url': '//path/to/saturn.jpg'
+//};
+//print(name);
+//}
