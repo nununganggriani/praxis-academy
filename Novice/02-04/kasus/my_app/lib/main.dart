0f6388a04341a3a714ff6dee7848ad918efc1b0f
nununganggriani <nunung1700016031@webmail.uad.ac.id>
2020-03-21 17:42:10 +0700

    update 02-04

--- Novice/02-04/kasus/my_app/lib/main.dart
+++ Novice/02-04/kasus/my_app/lib/main.dart
@@ -3,30 +3,69 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("B T S"),
-          backgroundColor: Colors.black,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.mail),
-              onPressed: () {},
-              color: Colors.purple,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hero Animation"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Hero(
+              tag: "logo",
+              child: Image.asset(
+                "asset/logo_bts21.png",
+                width: 150,
+                height: 150,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: RaisedButton(
+                child: Text("To another screen"),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return SecondScreen();
+                  }));
+                },
+              ),
             )
           ],
         ),
-        body: SafeArea(
-          child: Image(
-            image: AssetImage('images/bts.jpg'),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Another Screen"),
+      ),
+      body: Center(
+        child: Hero(
+          tag: "logo",
+          child: Image.asset(
+            "asset/bt21.png",
+            width: 75,
+            height: 75,
           ),
         ),
       ),
