74eddfc3b47f5a47dec4b501187887b922cc467b
nununganggriani <nunung1700016031@webmail.uad.ac.id>
2020-05-04 03:37:52 +0700

    update

---
+++ Novice/04-01/kasus/battery_level_app/android/app/src/main/kotlin/com/example/battery_level_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.battery_level_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
